66b42cc1200078bfdfc2df104a60a9f6e920ed86
yugiiiii740 <yugi3436@gmail.com>
2025-08-07 14:36:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
-const fs = require('fs');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers] });
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Basit bir HTTP server (Render bunu algılayacak)
+app.get('/', (req, res) => {
+  res.send('Bot is running!');
+});
+
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
+});
+
+// Discord client
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
 
 client.commands = new Collection();
 
-// Slash komutlarını yükle
-const commandFiles = fs.readdirSync('./commands');
+const fs = require('fs');
+
+// Komutları yükle
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   client.commands.set(command.data.name, command);
 }
 
 // Eventleri yükle
-const eventFiles = fs.readdirSync('./events');
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 for (const file of eventFiles) {
   const event = require(`./events/${file}`);
   client.on(event.name, (...args) => event.execute(...args, client));
